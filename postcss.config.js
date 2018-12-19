cf790eaeced29738b7043fda005bbaf9fe11d8b8
Kvkens <kvkens@qq.com>
2018-12-19 10:54:16 +0800

    add:postcss-flexbugs

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     plugins: [
         require('autoprefixer')({ browsers: ['last 10 Chrome versions', 'last 10 Firefox versions', 'Safari >= 7', 'ie > 10'] }),
+        require('postcss-flexbugs-fixes')
     ]
 }
